121670a4529405e30fa24e6ecf40d69de36a3dfb
Tobias Feld <tobias.feld@hotmail.com>
2021-03-10 00:35:07 +0100

    Add error message to page

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -2,8 +2,10 @@ import React, { cloneElement } from "react";
 import "../styles/page.scss";
 export default function Page(props) {
   return (
-    <div className="page">
-      {cloneElement(<>{props.children}</>, { ...props })}
+    <div className="pageContainer">
+      <div className="page">
+        {cloneElement(<>{props.children}</>, { ...props })}
+      </div>
     </div>
   );
 }
--- src/components/pages/finish.jsx
+++ src/components/pages/finish.jsx
@@ -4,6 +4,7 @@ import Page from "../page";
 export default function FinishPage(props) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     setLoading(true);
     var raw = JSON.stringify(props.data);
@@ -16,12 +17,20 @@ export default function FinishPage(props) {
     };
 
     fetch(GETURL() + "/api/submitSetup", requestOptions)
-      .then((response) => {
+      .then(async (response) => {
         if (response.ok) {
           setLoading(false);
           props.cb();
         } else {
+          try {
+          } catch (e) {
+            console.error(e);
+            setError(true);
+            props.reportError();
+          }
+          let json = await response.json();
           setError(true);
+          setErrorMessage(json.message);
           props.reportError();
         }
       })
@@ -31,12 +40,18 @@ export default function FinishPage(props) {
         props.reportError();
       });
   }, []);
-
+  let errorMsgElement = (
+    <h1 className="subtitle">
+      <b>Error message returned by the server:</b>
+      <br /> {errorMessage}
+    </h1>
+  );
   if (error) {
     return (
       <Page>
         <h1 className="title">An error occurred</h1>
         <h1 className="subtitle">Try again by reloading the page</h1>
+        {errorMessage && errorMsgElement}
         <button
           className="button is-danger"
           onClick={() => {
